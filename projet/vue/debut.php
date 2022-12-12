c06b8631bd6293d7be4bb7c72aaf6a91a525472b
Nathan GILBERT <74240315+Nathan2lim@users.noreply.github.com>
2022-12-12 12:25:35 +0100

    Delete projet directory

--- projet/vue/debut.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo $titre; ?> </title>
-    <link rel="stylesheet" href="../css/styleMenu.css">
-    <link rel="stylesheet" href="../css/styleGeneral.css">
-</head>
-<body>
--- projet/vue/utilisateur/lesUtilisateur.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-foreach($tab_u as $u)
-{
-    $u->afficher();
-}
-
-?>
